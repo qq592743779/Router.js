4f55b2eff6ae86bbbfc39da3f326bd12d9f524ea
Azu <592743779@qq.com>
2018-07-29 16:42:40 +0800

    简单的路由监听实现

---
+++ router.js
@@ -0,0 +1,22 @@
+class Router {
+    constructor(){
+        //  以键值对的形式存储路由
+        this.routes = {};
+        //  存储当前的路由
+        this.currentUrl = '';
+        this.refresh = this.refresh.bind(this);
+        window.addEventListener('load', this.refresh);
+        window.addEventListener('hashchange', this.refresh);
+    }
+    //  将path路径与对应路径的callback函数存储
+    route(path, callback){
+        this.routes[path] = callback || function() {};
+    }
+    //  刷新
+    refresh(){
+        //  获取当前URL的hash路径
+        this.currentUrl = window.location.hash.slice(1) || '/';
+        //  执行当前hash路径的callback函数
+        this.routes[this.currentUrl]();
+    }
+}
